c3e8ac43d9b0d538a12f259bc09499de6ff78d36
ViveTheModder <viveluvsgamening@gmail.com>
2024-10-28 23:58:04 +0100

    added Wii support

--- src/cmd/LittleEndian.java
+++ src/cmd/LittleEndian.java
@@ -7,6 +7,7 @@ public class LittleEndian
 {
 	public static float getFloat(float data)
 	{
+		if (MainApp.isForWii) return data;
 		ByteBuffer bb = ByteBuffer.allocate(4);
 		bb.asFloatBuffer().put(data);
 		bb.order(ByteOrder.LITTLE_ENDIAN);
@@ -14,6 +15,7 @@ public class LittleEndian
 	}
 	public static int getInt(int data)
 	{
+		if (MainApp.isForWii) return data;
 		ByteBuffer bb = ByteBuffer.allocate(4);
 		bb.asIntBuffer().put(data);
 		bb.order(ByteOrder.LITTLE_ENDIAN);
@@ -21,6 +23,7 @@ public class LittleEndian
 	}
 	public static short getShort(short data)
 	{
+		if (MainApp.isForWii) return data;
 		ByteBuffer bb = ByteBuffer.allocate(2);
 		bb.asShortBuffer().put(data);
 		bb.order(ByteOrder.LITTLE_ENDIAN);
--- src/cmd/MainApp.java
+++ src/cmd/MainApp.java
@@ -1,4 +1,5 @@
 package cmd;
+//Tenkaichi Destruction Point Editor v1.1 by ViveTheModder
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -7,7 +8,9 @@ import java.util.Scanner;
 
 public class MainApp 
 {
+	static boolean isForWii, hasReadArg, hasWriteArg;
 	static RandomAccessFile res; //res is short for resident (not the evil one)
+	static String filename;
 	static final int CHARA_COUNT = 161;
 	static final String CSV_PATH = "./csv/";
 	static final String IN_PATH = "./in/";
@@ -16,11 +19,16 @@ public class MainApp
 		String output="chara-name,chara-dp\n";
 		File charaCSV = new File(CSV_PATH+"characters.csv");
 		Scanner sc = new Scanner(charaCSV);
+		
+		res.seek(4);
+		int progressCharaParamOffset = LittleEndian.getInt(res.readInt());
+		int offsetOfFirstDP = progressCharaParamOffset+12;
+		
 		for (int i=0; i<CHARA_COUNT; i++)
 		{
 			String input = sc.nextLine();
 			String name = input.split(",")[1];
-			int pos=(60*i)+44; res.seek(pos);
+			int pos=(60*i)+offsetOfFirstDP; res.seek(pos);
 			short dp = LittleEndian.getShort(res.readShort());
 			output+=name+","+dp+"\n";
 		}
@@ -29,32 +37,10 @@ public class MainApp
 		FileWriter outputWriter = new FileWriter(outputCSV);
 		outputWriter.write(output);
 		outputWriter.close();
+		System.out.println(outputCSV.getName()+" made successfully!");
 	}
-	public static void setDP() throws IOException
+	public static void initFunctionality() throws IOException
 	{
-		File dpCSV = new File(CSV_PATH+"dp-list.csv");
-		Scanner sc = new Scanner(dpCSV);
-		sc.nextLine(); //skip header
-		while (sc.hasNextLine())
-		{
-			String input = sc.nextLine();
-			String[] inputArray = input.split(",");
-			int charaID = Integer.parseInt(inputArray[0]);
-			short newDP = Short.parseShort(inputArray[1]);
-			int pos=(60*charaID)+44; res.seek(pos);
-			//RAF write methods always write in big endian, so the value is turned to big endian beforehand to reverse the process
-			newDP = LittleEndian.getShort(newDP);
-			res.writeShort(newDP);
-		}
-		sc.close();
-	}
-	public static void main(String[] args) throws IOException
-	{
-		if (args.length==0)
-		{
-			System.out.println("No arguments detected. Please enter an argument that is either -r or -w.");
-			System.exit(1); //faulty termination
-		}
 		File folder = new File("./in/");
 		File[] files = folder.listFiles();
 		int fileCnt = files.length;
@@ -64,16 +50,78 @@ public class MainApp
 		for (int i=0; i<fileCnt; i++)
 		{
 			res = resFiles[i];
-			String filename = files[i].getName();
+			filename = files[i].getName();
 			//I would have preferred using regex for this, but my stopwatch said no
 			if (filename.endsWith(".unk")) filename=filename.replace(".unk", "");
 			else filename=filename.replace(".pak", "");
-			if (args[0].equals("-r")) exportCSV(filename);
-			else if (args[0].equals("-w")) setDP();
+			if (hasReadArg==true) exportCSV(filename);
+			else if (hasWriteArg==true) setDP();
+		}
+	}
+	public static void readFirstArg(String[] args) throws IOException
+	{
+		if (args[0].equals("-r")) 
+		{
+			hasReadArg=true; initFunctionality();
+		}
+		else if (args[0].equals("-w")) 
+		{
+			hasWriteArg=true; initFunctionality();
+		}
+		else
+		{
+			System.out.println("Invalid argument detected. Please enter an argument that is either -r or -w.");
+			System.exit(3);
+		}
+	}
+	public static void setDP() throws IOException
+	{
+		File dpCSV = new File(CSV_PATH+"dp-list.csv");
+		Scanner sc = new Scanner(dpCSV);
+		sc.nextLine(); //skip header
+		
+		res.seek(4);
+		int progressCharaParamOffset = LittleEndian.getInt(res.readInt());
+		int offsetOfFirstDP = progressCharaParamOffset+12;
+
+		while (sc.hasNextLine())
+		{
+			String input = sc.nextLine();
+			String[] inputArray = input.split(",");
+			int charaID = Integer.parseInt(inputArray[0]);
+			short newDP = Short.parseShort(inputArray[1]);
+			int pos=(60*charaID)+offsetOfFirstDP; res.seek(pos);
+			//RAF write methods always write in big endian, so the value is turned to big endian beforehand to reverse the process
+			newDP = LittleEndian.getShort(newDP);
+			res.writeShort(newDP);
+		}
+		System.out.println(filename+" overwritten successfully!");
+		sc.close();
+	}
+	public static void main(String[] args) throws IOException
+	{
+		if (args.length>2)
+		{
+			System.out.println("Too many arguments provided. Please only provide up to 2 arguments.");
+			System.exit(1);
+		}
+		else
+		{
+			if (args.length==2)
+			{
+				if (!args[1].equals("-wii"))
+				{
+					System.out.println("Invalid argument detected. 2nd argument can only be -wii.");
+					System.exit(2);
+				}
+				else isForWii=true;
+				readFirstArg(args);
+			}
+			else if (args.length==1) readFirstArg(args);
 			else
 			{
-				System.out.println("Invalid argument detected. Please enter an argument that is either -r or -w.");
-				System.exit(2); //faulty termination
+				System.out.println("No arguments detected. Please enter an argument that is either -r or -w.");
+				System.exit(4);
 			}
 		}
 	}
